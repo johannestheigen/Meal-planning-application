6df647ec2bf81a24564e870e5d6e20cdc58e780a
Johannes Nupen Theigen <johannes.theigen@gmail.com>
2024-11-04 15:28:31 +0100

    Add UiPrintHandler.Java

--- src/main/java/edu/ntnu/idi/bidata/printutility/UiPrintHandler.java
+++ src/main/java/edu/ntnu/idi/bidata/printutility/UiPrintHandler.java
@@ -1,9 +1,13 @@
 package edu.ntnu.idi.bidata.printutility;
 
 /**
- * Handles
+ * Handles everything there is to print in the main
+ * interface.
+ *
+ * @author Johannes Nupen Theigen
+ * @version 0.0.1
+ * @since 11.04.2024
  */
-public class PrintHandler
-{
+public class UiPrintHandler {
 
 }
