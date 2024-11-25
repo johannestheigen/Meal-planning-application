580779788ba80548aa708d186ddea9c6a3df50bf
Johannes Nupen Theigen <johannes.theigen@gmail.com>
2024-11-25 17:25:50 +0100

    Rename Launch back to main

--- src/main/java/edu/ntnu/idi/bidata/app/Main.java
+++ src/main/java/edu/ntnu/idi/bidata/app/Main.java
@@ -1,14 +1,14 @@
 package edu.ntnu.idi.bidata.app;
 
 /**
- * <p>The main starting point of your application. Let this class create the
+ * <p>The main starting point of the application. Let this class create the
  * instance of your main-class that starts your application.</p>
 
  * @author Johannes Nupen Theigen
- * @version 0.0.3
- * @since 11.21.2024
+ * @version 0.0.4
+ * @since 11.25.2024
  */
-public class Launch {
+public class Main {
   /**
    * <p>The main method that serves as the entry point for the application.
    * It initializes the UserInterface and starts the interaction process.</p>
