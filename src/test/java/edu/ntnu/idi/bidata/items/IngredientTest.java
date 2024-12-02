b60fa55a9321e16f21d49b707b50a66aac745b93
Johannes Nupen Theigen <johannes.theigen@gmail.com>
2024-12-02 21:05:46 +0100

    IngredientTest
    - Improve the documentation of the class
    significantly and the names of the test methods.
    
    - The other test classes will be made and improved later.

--- src/test/java/edu/ntnu/idi/bidata/items/IngredientTest.java
+++ src/test/java/edu/ntnu/idi/bidata/items/IngredientTest.java
@@ -1,21 +1,18 @@
-/**
- * This class contains unit tests for the Ingredient class.
- * It tests the functionality of the Ingredient methods
- * including setIngredientName, setAmount, and setExpirationDate.
- */
-
 package edu.ntnu.idi.bidata.items;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 import java.time.LocalDate;
 
+/**
+ * <p>Test class for the Ingredient class.
+ */
 class IngredientTest {
 
   Ingredient testIngredient;
 
   /**
-   * Creates a test object of Ingredient that will be used throughout the entire test.
+   * <p>Initializes a new instance of the Ingredient class before each test.</p>
    */
   @BeforeEach
   void beforeEach() {
@@ -23,64 +20,91 @@ class IngredientTest {
   }
 
   /**
-   * Test that checks if a valid input of ingredientName is set correctly.
+   * <p>Test that checks if a valid input of ingredientName is set correctly.</p>
    */
   @Test
-  void setIngredientNameTestForPositiveInput() {
+  void setIngredientNameTestPositiveTest() {
     testIngredient.setName("Tomato");
     assertEquals("Tomato", testIngredient.getName(), "The output should be Tomato");
   }
 
+  /**
+   * <p>Test that checks that the ingredientName is not incorrectly set</p>
+   */
   @Test
-  void setIngredientNameTestForNegativeInput() {
+  void setIngredientNameTestNegativeTest() {
     testIngredient.setName("Tomato");
-    assertNotEquals("Tomata",testIngredient.getName(),"The output should not be Tomata.");
+    assertNotEquals("Apple",testIngredient.getName(),"The output should not be Apple.");
   }
 
+  /**
+   * <p>Test that checks if a valid input of quantity is set correctly.</p>
+   */
   @Test
-  void setAmountForPositiveInput() {
+  void setQuantityPositiveTest() {
     testIngredient.setQuantity(10);
     assertEquals(10,testIngredient.getQuantity(),"The output should be 10");
   }
 
+  /**
+   * <p>Test that checks that the quantity is not incorrectly set.</p>
+   */
   @Test
-  void setAmountForNegativeInput() {
+  void setQuantityNegativeTest() {
     testIngredient.setQuantity(100);
     assertNotEquals(-50,testIngredient.getQuantity(),"The output should not -50");
   }
 
+  /**
+   * <p>Test that checks if a valid input of unit is set correctly.</p>
+   */
   @Test
-  void setUnitForPositiveInput() {
+  void setUnitPositiveTest() {
     testIngredient.setUnit("kg");
     assertEquals("kg",testIngredient.getUnit(),"The output should be kg");
   }
 
+  /**
+   * <p>Test that checks that the unit is not incorrectly set.</p>
+   */
   @Test
-  void setUnitForNegativeInput() {
+  void setUnitNegativeTest() {
     testIngredient.setUnit("kg");
     assertNotEquals("mg",testIngredient.getUnit(),"The output should not be g");
   }
 
+  /**
+   * <p>Test that checks if a valid input of price is set correctly.</p>
+   */
   @Test
-  void setPriceForPositiveInput() {
+  void setPricePositiveTest() {
     testIngredient.setPrice(1);
     assertEquals(1,testIngredient.getPrice(),"The output should be 1.");
   }
 
+  /**
+   * <p>Test that checks that the price is not incorrectly set.</p>
+   */
   @Test
-  void setPriceForNegativeInput() {
+  void setPriceNegativeTest() {
     testIngredient.setPrice(5);
     assertNotEquals(9,testIngredient.getPrice(), "The output should not be 9");
   }
 
+  /**
+   * <p>Test that checks if a valid input of expirationDate is set correctly.</p>
+   */
   @Test
-  void setExpirationDateForPositiveInput() {
+  void setExpirationDatePositiveTest() {
     testIngredient.setExpirationDate(LocalDate.of(2035,1,1));
     assertEquals(LocalDate.of(2035,1,1),testIngredient.getExpirationDate(),"The output should be 2035 1 1");
   }
 
+  /**
+   * <p>Test that checks that the expirationDate is not incorrectly set</p>
+   */
   @Test
-  void setExpirationDateForNegativeInput() {
+  void setExpirationDateNegativeTest() {
     testIngredient.setExpirationDate(LocalDate.of(2037,6,17));
     assertNotEquals(LocalDate.of(2029,1,1),testIngredient.getExpirationDate(),"The output should not be 2029 1 1");
   }
